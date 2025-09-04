dff14413bc253d96bdfcf5e1366cbe41990386bd
Hasmukh <mac@Hasmukhs-MacBook-Air.local>
2025-09-04 23:47:04 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -23,9 +24,9 @@ import demo.utils.ExcelDataProvider;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
-public class TestCases extends ExcelDataProvider{ // Lets us read the data
+public class TestCases extends ExcelDataProvider { // Lets us read the data
         ChromeDriver driver;
-
+        // Wrappers wapperClass = new Wrappers();
         /*
          * TODO: Write your tests here with testng @Test annotation.
          * Follow `testCase01` `testCase02`... format or what is provided in
@@ -56,7 +57,148 @@ public class TestCases extends ExcelDataProvider{ // Lets us read the data
                 driver = new ChromeDriver(options);
 
                 driver.manage().window().maximize();
+                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+                 
         }
+
+        @Test(enabled = true)
+        public void testCase01() {
+              driver.get("https://www.youtube.com/");
+
+                WebElement aboutLink = driver.findElement(By.xpath("//a[contains(text(), 'About')]"));
+                try {
+
+                        if (!aboutLink.isDisplayed()) {
+                                // Thread.sleep(1000);
+                                WebElement menu_Button = driver.findElement(By.xpath(
+                                                "(//span[contains(@class, 'yt-icon-shape style-scope yt-icon yt-spec-icon-shape')])[1]"));
+                                Wrappers.clickOnbuttonJS(driver, menu_Button);
+                                Wrappers.clickOnbuttonJS(driver, aboutLink);
+                        } else {
+                                // Thread.sleep(1000);
+                                Wrappers.clickOnbuttonJS(driver, aboutLink);
+                        }
+                        WebElement titleElement = driver.findElement(
+                                        By.xpath("//h1[contains(normalize-space(text()),'About YouTube')]"));
+                        boolean status = titleElement.isDisplayed();
+                        Assert.assertEquals(status, true);
+
+                        System.out.println("titleElement :- " + titleElement.getText());
+                        // Thread.sleep(10000);
+                } catch (Exception e) {
+                        // TODO: handle exception
+                        System.out.println("About Element is not Visisble " + e.getMessage());
+                }
+
+        }
+
+        @Test(enabled = true)
+        public void testCase02() throws Throwable {
+                driver.get("https://www.youtube.com/");
+                System.out.println("Testcase 2 Starting...");
+
+                SoftAssert softAssert = new SoftAssert();
+
+                Thread.sleep(5000);
+                WebElement movieElement = driver.findElement(By
+                                .xpath("//yt-formatted-string[contains(text(),'Films') or contains(text(),'Movie')]"));
+                Wrappers.clickOnbuttonJS(driver, movieElement);
+                Thread.sleep(5000);
+                WebElement topSellingElement = driver.findElement(By.xpath("//span[@id='title']"));
+
+                boolean topSeEl = topSellingElement.getText().equals("Top selling");
+                softAssert.assertEquals(topSeEl, true);
+                Thread.sleep(5000);
+
+                WebElement nextArrorElement = driver.findElement(By.xpath(
+                                "(.//ytd-button-renderer[contains(@class,'style-scope yt-horizontal-list-renderer arrow')])[2]"));
+                Wrappers.clickonlast(driver, nextArrorElement);
+                WebElement lastThumbElement = driver.findElement(By.xpath(
+                                ".//ytd-grid-movie-renderer[contains(@class,'style-scope yt-horizontal-list-renderer')][last()]"));
+                boolean status1 = lastThumbElement.isDisplayed();
+                WebElement movieMark = driver.findElement(By.xpath(
+                                "(.//div[contains(@class,'badge  badge-style-type-simple style-scope ytd-badge-supported-renderer style-scope ytd-badge-supported-renderer')])[last()]"));
+
+                String movieMarkString = movieMark.getText();
+                softAssert.assertEquals(
+                                movieMarkString.equalsIgnoreCase("U/A") ||
+                                                movieMarkString.equalsIgnoreCase("U") ||
+                                                movieMarkString.equalsIgnoreCase("A"),
+                                true);
+                WebElement drameElement = driver.findElement(By.xpath(
+                                ".//ytd-grid-movie-renderer[contains(@class,'style-scope yt-horizontal-list-renderer')][last()]//span[starts-with(@class,\"grid\")]"));
+
+                String DramStr = Wrappers.getSpitLeftString(driver, drameElement);
+
+                softAssert.assertEquals(status1, true);
+                softAssert.assertEquals(
+                                DramStr.equalsIgnoreCase("Drama") ||
+                                                DramStr.equalsIgnoreCase("Animation") ||
+                                                DramStr.equalsIgnoreCase("Comedy"),
+                                true);
+
+                softAssert.assertAll();
+        }
+
+        @Test(enabled = true)
+        public void testCase03() {
+
+                try {
+                       // driver.get("https://www.youtube.com/");
+                        System.out.println("Start Testcases 3 :-");
+                        WebElement musicElement = driver.findElement(By.xpath("//a[@title='Music']"));
+                      
+                       
+                        Wrappers.clickOnbutton(driver, musicElement);
+                         Thread.sleep(3000);
+                          WebElement showmorWebElement = driver.findElement(By.xpath("//span[contains(text(),\"India's Biggest Hits\")]/ancestor::div[@id='dismissible']//yt-button-shape//span[text()='Show more']"));
+                          Wrappers.clickOnbuttonJS(driver, showmorWebElement);
+    
+                        By locate_Elemnent = By.xpath(
+                                        "//span[contains(text(),\"India's Biggest Hits\")]/ancestor::div[@id='dismissible']");
+                        int soungCount = Wrappers.movieDetails(driver, locate_Elemnent);
+                        System.out.println("soungCount" + soungCount);
+                        SoftAssert sa = new SoftAssert();
+                        sa.assertTrue(soungCount <= 50);
+                        sa.assertAll();
+                } catch (Exception e) {
+                        // TODO: handle exception
+                        System.out.println(" Testcases 3 :- Fail");
+                }
+                System.out.println("End Testcases 3");
+
+        }
+        @Test(enabled =true)
+        public void testCase04(){
+                try {
+                   //     driver.get("https://www.youtube.com/");
+                        System.out.println("Starting Testcase 4...");
+                        WebElement newsElement = driver.findElement(By.xpath("//a[@title='News']"));
+                       Wrappers.clickOnbutton(driver, newsElement);
+                         Thread.sleep(3000);
+                        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                     WebElement containsCards= wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='rich-shelf-header-container' and contains(.,'Latest news posts')]//ancestor::div[1]//div[@id='contents']")));
+                       
+                      Thread.sleep((new java.util.Random().nextInt(3)+2)*1000); 
+                      long sumofVotes = 0;
+                      for(int i =0;i<3;i++){
+                       System.out.println( Wrappers.findElementAndshowme(driver, By.xpath("//div[@id='rich-shelf-header-container' and contains(.,'Latest news posts')]//ancestor::div[1]//div[@id='contents']//div[@id='header']"), containsCards, i));
+                       System.out.println( Wrappers.findElementAndshowme(driver, By.xpath("//div[@id='rich-shelf-header-container' and contains(.,'Latest news posts')]//ancestor::div[1]//div[@id='contents']//div[@id='body']"), containsCards, i));
+                       String res= Wrappers.findElementAndshowme(driver, By.xpath("//div[@id='rich-shelf-header-container' and contains(.,'Latest news posts')]//ancestor::div[1]//div[@id='contents']//span[@id='vote-count-middle']"), containsCards, i);
+
+                        sumofVotes = sumofVotes + Wrappers.convertLongValue(res);
+
+                        Thread.sleep((new java.util.Random().nextInt(3)+2)*1000); 
+                      }
+                      System.out.println("sumofVotes. "+sumofVotes);
+                        System.out.println("Ends Testcase 4");
+                        
+                } catch (Exception e) {
+                        // TODO: handle exception
+                        System.out.println("Testcase 4 is Failed");
+                }
+        }
+
 
         @AfterTest
         public void endTest() {
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,5 +1,6 @@
 package demo.wrappers;
 
+import org.checkerframework.checker.units.qual.t;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
@@ -9,9 +10,145 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.List;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+   /*
+    * Write your selenium wrappers here
+    */
+   WebDriver driver;
+
+
+   public Wrappers(WebDriver driver) {
+       this.driver = driver;
+   }
+
+
+   public static void clickOnbutton(WebDriver driver, WebElement element) {
+       try {
+           WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+           wait.until(ExpectedConditions.visibilityOf(element));
+           if (element != null) {
+               element.click();
+           }
+
+
+       } catch (Exception e) {
+           // TODO: handle exception
+           System.out.println("Element is not clickable " + e.getMessage());
+       }
+   }
+
+
+   public static void clickOnbuttonJS(WebDriver driver, WebElement element) {
+       try {
+          // Thread.sleep(5000);
+           WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+           wait.until(ExpectedConditions.visibilityOf(element));
+           if (element != null) {
+               JavascriptExecutor js = (JavascriptExecutor) driver;
+               js.executeScript("arguments[0].click();", element);
+           }
+       } catch (Exception e) {
+           // TODO: handle exception
+       }
+
+
+   }
+   public static void clickonlast(WebDriver driver, WebElement element){
+
+
+       try {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+           wait.until(ExpectedConditions.visibilityOf(element));
+           while (element.isDisplayed()) {
+               Thread.sleep(5000);
+               element.click();
+           }
+          
+       } catch (Exception e) {
+           // TODO: handle exception
+            System.out.println("Not Get last Element in movies "+ e.getMessage());
+       }
+
+
+   }
+
+   public static String getSpitLeftString(WebDriver driver, WebElement element){
+     String lefString = "";
+    try {
+        
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+           wait.until(ExpectedConditions.visibilityOf(element));
+           if (element.isDisplayed()) {
+               Thread.sleep(5000);
+               String fullSting = element.getText();
+             String[] partsString = fullSting.split("•");
+            lefString = partsString[0].trim(); 
+            System.out.println(lefString);
+             return lefString;
+           }
+
+    } catch (Exception e) {
+        // TODO: handle exception
+        System.out.println("Not Get Left String in Whole String "+ e.getMessage());
+    }
+    return lefString;
+   }
+
+   public static int movieDetails(WebDriver driver , By locateby) throws InterruptedException{
+    WebElement songsElement = driver.findElement(locateby);
+    
+    Thread.sleep(3000);
+    
+    List<WebElement> musicList = songsElement.findElements(By.xpath(".//div[@id='content']"));
+    String playlistName=musicList.get(4).findElement(By.xpath(".//h3[contains(@class,'model__heading')]")).getText();
+    System.out.println("PlaylistName: "+playlistName);
+    String mostrightSongsCount= musicList.get(4).findElement(By.xpath(".//div[contains(@class,'yt-badge-shape__text')]")).getText();
+    return Integer.parseInt(mostrightSongsCount.split(" ")[0]);
+
+   }
+
+   public static String findElementAndshowme(WebDriver driver, By locator , WebElement we , int elementNo){
+   
+    List<WebElement> elements = we.findElements(locator);
+    String txt="";
+    for (WebElement webElement : elements) {
+        txt = elements.get(elementNo).getText();
+    }
+    return txt;
+   }
+
+   public static long convertLongValue(String value){
+
+    value = value.trim().toUpperCase();
+    char lastcharvalue= value.charAt(value.length()-1);
+    int multiple=1;
+    switch (lastcharvalue) {
+        case 'K':
+            multiple=1000;
+            break;
+         case 'M':
+            multiple=1000000;
+            break;  
+        case 'B':
+            multiple=1000000000;
+            break;        
+    
+        default:
+            if(Character.isDigit(lastcharvalue)){
+                return Long.parseLong(value);
+            }
+            throw new IllegalArgumentException(value);
+    }
+    String numbricPart= value.substring(0, value.length()-1);
+    double number = Double.parseDouble(numbricPart);
+
+    return (long) (number *multiple);
+
+   }
+
+
+
 }
+
